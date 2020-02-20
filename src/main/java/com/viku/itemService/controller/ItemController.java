8547d063b7299533db1bcce0fdb5feca064161a1
pankajsharma <yellow*99>
2020-02-21 01:05:51 +0530

    fixing my chutiyappa

--- src/main/java/com/viku/itemService/controller/ItemController.java
+++ src/main/java/com/viku/itemService/controller/ItemController.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Slf4j
+@RestController
 public class ItemController {
 
     @Autowired
